e9b9dcbbe00fb5c7db39578cabea740b191401cc
isayhc <isayahculbertson@gmail.com>
2023-12-06 22:47:46 -0500

    seeting up embeddind

---
+++ ziliz_milvrus.py
@@ -0,0 +1,4 @@
+# https://colab.research.google.com/github/truera/trulens/blob/main/trulens_eval/examples/expositional/vector-dbs/milvus/milvus_evals_build_better_rags.ipynb
+# https://colab.research.google.com/github/truera/trulens/blob/main/trulens_eval/examples/expositional/vector-dbs/milvus/milvus_simple.ipynb
+
+from llama_index.vector_stores import MilvusVectorStore
