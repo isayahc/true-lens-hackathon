90ff42061cb62244488ea4bb93611e97c19b6865
isayhc <isayahculbertson@gmail.com>
2023-12-06 20:13:56 -0500

    added url to code source

--- truEra-litellm-vertex-sample.py
+++ truEra-litellm-vertex-sample.py
@@ -9,19 +9,24 @@ from trulens_eval import TruChain, Feedback, Tru #, LiteLLM
 from google.cloud import aiplatform
 from litellm import completion
 
+#######################################################################
+# RESOURCES
+#############
+# https://github.com/truera/trulens/blob/main/trulens_eval/examples/expositional/models/google_vertex_quickstart.ipynb
+
+#######################################################################
+
 
 tru = Tru()
 tru.reset_database()
 
-# LiteLLM.vertex_project = "vectara-404518"
-# LiteLLM.vertex_location = "us-central1"
 
 aiplatform.init(
     project = "vectara-404518",
     location="us-central1"
 )
 
-feedback_model = LiteLLM(model_engine="chat-bison")
+feedback_model:LiteLLM = LiteLLM(model_engine="chat-bison")
 
 response = completion(model="chat-bison", messages=[{"role": "user", "content": "write code for saying hi from LiteLLM"}])
 
@@ -37,7 +42,7 @@ full_prompt = HumanMessagePromptTemplate(
 
 chat_prompt_template = ChatPromptTemplate.from_messages([full_prompt])
 
-llm = OpenAI(temperature=0.9, max_tokens=128)
+llm:OpenAI = OpenAI(temperature=0.9, max_tokens=128)
 
 chain = LLMChain(llm=llm, prompt=chat_prompt_template, verbose=True)
 
